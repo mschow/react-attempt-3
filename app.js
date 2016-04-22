4e2a2d9ac8f807952178825bd858ab7a6faa3649
Micah Schow <micah.schow@gmail.com>
2016-04-22 13:37:45 -0600

    Adds API routing with express.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 'use strict'
 
-import express from 'express'
 import config from 'config'
+import express from 'express'
+import api from './routes'
 
 const app = express()
 const PORT = config.port
+
+app.use('/api', api)
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
